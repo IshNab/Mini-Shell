f41e319c3fec30d2ab81835d77da266bc1173697
Ishta <ishtnabakka@gmail.com>
2025-09-11 13:00:40 +0200

    modified .h file to match changed builtin files

---
+++ inc/parser.h
@@ -0,0 +1,14 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+int parse_command(char *input);
+
+int builtin_cd(char **args);
+int builtin_echo(char **args);
+int builtin_env(char **args);
+int builtin_exit(char **args);
+int builtin_export(char **args);
+int builtin_pwd(char **args);
+int builtin_unset(char **args);
+
+#endif
