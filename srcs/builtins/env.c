fd2d040e04b51bbd804fc82883e2deef160ae4d0
Ishta Nabakka <157944465+IshNab@users.noreply.github.com>
2025-09-18 13:48:30 +0000

    everything complies and creates executable, may have lost history function

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 
 extern char **environ;
-
+/*
 static int	ft_strcmp(const char *s1, const char *s2)
 {
 	size_t	i;
@@ -14,7 +14,7 @@ static int	ft_strcmp(const char *s1, const char *s2)
 	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
+}*/
 
 static void	print_env_vars(void)
 {
