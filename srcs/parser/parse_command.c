4ebb8c4d05ecb542244d8aaf9cd8838dfd79b041
Marina Borges Sarno <maborges@student.42berlin.de>
2025-09-19 18:43:48 +0200

    norm fixes

--- srcs/parser/parse_command.c
+++ srcs/parser/parse_command.c
@@ -6,33 +6,32 @@
 /*   By: maborges <maborges@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:35:26 by maborges          #+#    #+#             */
-/*   Updated: 2025/09/19 14:38:30 by maborges         ###   ########.fr       */
+/*   Updated: 2025/09/19 18:30:43 by maborges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int parse_command(char *input) {
-    // Simple parser: split input and call built-in if matched
-    char *args[100];
-    int argc = 0;
-    char *token = strtok(input, " \t\n");
-    while (token && argc < 99) {
-        args[argc++] = token;
-        token = strtok(NULL, " \t\n");
-    }
-    args[argc] = NULL;
-
-    if (argc == 0) return 0;
-
-    if (strcmp(args[0], "cd") == 0) return builtin_cd(args); //strcmp not allowed
-    if (strcmp(args[0], "echo") == 0) return builtin_echo(args);
-    if (strcmp(args[0], "env") == 0) return builtin_env(args);
-    if (strcmp(args[0], "exit") == 0) return builtin_exit(args);
-    if (strcmp(args[0], "export") == 0) return builtin_export(args);
-    if (strcmp(args[0], "pwd") == 0) return builtin_pwd(args);
-    if (strcmp(args[0], "unset") == 0) return builtin_unset(args);
-
-    // Not a built-in
-    return -1;
+// Simple parser: split input and call built-in if matched
+int	parse_command(char *input)
+{
+	char	*args[100];
+	int		argc = 0;
+	char	*token = strtok(input, " \t\n"); // strtok not allowed
+	while (token && argc < 99)
+	{
+		args[argc++] = token;
+		token = strtok(NULL, " \t\n");
+	}
+	args[argc] = NULL;
+	if (argc == 0) return 0;
+	if(strcmp(args[0], "cd") == 0) return builtin_cd(args); //strcmp not allowed
+	if(strcmp(args[0], "echo") == 0) return builtin_echo(args);
+	if (strcmp(args[0], "env") == 0) return builtin_env(args);
+	if (strcmp(args[0], "exit") == 0) return builtin_exit(args);
+	if (strcmp(args[0], "export") == 0) return builtin_export(args);
+	if (strcmp(args[0], "pwd") == 0) return builtin_pwd(args);
+	if (strcmp(args[0], "unset") == 0) return builtin_unset(args);
+	// Not a built-in
+	return -1;
 }
