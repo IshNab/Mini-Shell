42874a89ad444f60ac4f026586bf78f9833a5f31
Ishta <inabakka@student.42berlin.de>
2025-09-22 16:18:34 +0200

    making parsing command

--- srcs/utils.c
+++ srcs/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maborges <maborges@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: inabakka <inabakka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 16:35:22 by maborges          #+#    #+#             */
-/*   Updated: 2025/09/03 16:49:56 by maborges         ###   ########.fr       */
+/*   Updated: 2025/09/22 15:58:02 by inabakka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,37 @@ void	setup_signals(void)
 {
 	signal(SIGINT, signal_handler);
 	signal(SIGQUIT, SIG_IGN);
+}
+
+char *ft_strtok(char *str, const char *delim)
+{
+    static char *save_ptr = NULL;
+    char *token_start;
+    int i;
+
+    if (str)
+        save_ptr = str;
+    if (!save_ptr || *save_ptr == '\0')
+        return NULL;
+
+    // 跳过前导分隔符
+    while (*save_ptr && strchr(delim, *save_ptr))
+        save_ptr++;
+    if (*save_ptr == '\0')
+        return NULL;
+
+    token_start = save_ptr;
+    // 查找下一个分隔符
+    while (*save_ptr && !strchr(delim, *save_ptr))
+        save_ptr++;
+    if (*save_ptr)
+    {
+        *save_ptr = '\0';
+        save_ptr++;
+    }
+    else
+    {
+        save_ptr = NULL;
+    }
+    return token_start;
 }
