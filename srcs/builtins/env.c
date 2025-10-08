9f273b5852c09a244016c5388b63f7cadb65f659
urubu <marina.sarno@gmail.com>
2025-10-08 12:40:27 +0200

    fixed builtin_env()

--- srcs/builtins/env.c
+++ srcs/builtins/env.c
@@ -6,19 +6,27 @@
 /*   By: maborges <maborges@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/19 14:28:47 by maborges          #+#    #+#             */
-/*   Updated: 2025/10/07 16:57:07 by maborges         ###   ########.fr       */
+/*   Updated: 2025/10/08 12:39:50 by maborges         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-extern char **environ; // can we use this?
-
-int builtin_env(char **args, t_mshell *shell)
+int	builtin_env(char **args, t_mshell *shell)
 {
+	t_env	*current;
+
+	(void)args;
 	if (args)
 		error_msg("env gets no args", 1, shell);
-	for (char **e = environ; e && *e; ++e)
-        puts(*e);
-    return 0;
+	current = shell->env;
+	while (current)
+	{
+		ft_putstr_fd(current->key, 1);
+		ft_puchar_fd('=', 1);
+		ft_putstr_fd(current->value, 1);
+		ft_putchar_fd('\n', 1);
+		current = current->next;
+	}
+	return (0);
 }
